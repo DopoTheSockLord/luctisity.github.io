a87ecd94f82d1995871cc4cb18e001bfe75cf8c3
DopoTheSockLord <75450911+DopoTheSockLord@users.noreply.github.com>
2020-12-06 14:04:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta charset="UTF-8">
+		<title>Luctisity - Main</title>
+		<link rel="stylesheet" href="style.css">
+		<script src="http://host3958.itelit.pro/luctisity/script.js"></script>
+	</head>
+	
+	<body>
+		<!--<div class="header">
+			<a href="http://host3958.itelit.pro/luctisity/"><img src="img/icon2.png" width="150px" class="icon"></img></a>
+			<div class="searcharea">
+				<input type="search" placeholder="Search" class="search">
+				<img src="img/mag_glass.png" width="35px" class="searchbtn"></img>
+			</div>
+			<div class="headbuttons">
+				<a class="buttonhead" href><b>Create</b></a>
+				<a class="buttonhead" href><b>Explore</b></a>
+				<a class="buttonhead" href><b>Ideas</b></a>
+				<a class="buttonhead" href="credits.php"><b>About</b></a>
+			</div>
+			<div class="headbuttons">
+				<a class="headbuttonsother" href><img src="img/nav-notifications.png" width="30px" style="padding-top:3px;"></a>
+				<a class="headbuttonsother" href><img src="img/mystuff.png" width="30px"></a>
+				<a class="headbuttonsother" href><img src="img/settings.png" width="30px" style="padding-bottom:6px;"></a>
+			</div>
+			<div class="headeracc">
+				<!-- <li class="accountdrop"><a href="#"><img style="margin-bottom:-12px;" width="40px" src="img/defaultuser_60x60.png"> <label id="username"><b>demo_account</b></label></a>
+					  <ul class="dropdown">
+						<li id="accountdrop"><a href="">Профиль</a></li><br/>
+						<li id="accountdrop"><a href="">Мои работы</a></li><br/>
+						<li id="accountdrop"><a href="">Настройки учётной записи</a></li><br/>
+						<li id="accountdrop"><a href="">Выйти</a></li><br/>
+					  </ul>
+				</li>
+				<a href="#" id="MyProfileLink2"><img style="margin-bottom:-12px;" width="40px" src="img/defaultuser_60x60.png"> <label id="username"><b>demo_account</b></label></a>
+			</div><br/>
+		</div>
+		
+		<br/><br/><br/><br/>-->
+		<main>
+			<div class="newsfeed">
+				<div class="feed" style="height:250px;">
+					<label id="title" style="color:#141452;">What's happening?</label><br/>
+					<hr color="white"/>
+					<img width="40px" src="img/defaultuser_60x60.png"> <label style="color:#141452"><b style="color:#141452;">demo_account</b> was promoted to Leader</label>
+				</div>
+				<div class="feed" style="margin-left:380px; margin-top:-272px; height:250px;" height="450px">
+					<label id="title" style="color:#141452;">News</label><br/>
+					<hr color="white"/>
+					<img width="50px" src="img/newsrocket.png"><label style="color:#141452;"><b style="color:#141452;">02.10.2020 - </b> Welcome to Luctisity, users!~</label><br/>
+					<img width="50px" src="img/newsrocket.png"><label style="color:#141452;"><b style="color:#141452;">26.09.2020 - </b> Happy anniversary, RSCServer!</label><br/>
+					<img width="50px" src="img/newsrocket.png"><label style="color:#141452;"><b style="color:#141452;">26.09.2019 - </b> RSCServer's official launch!</label>
+				</div>
+			</div>
+			<div class="projectscontainer" style="width:810px; display:block; margin-left:auto; margin-right:auto;">
+				<label id="title" style="color:#ccccff;">Featured Projects</label><br/>
+				<hr color="white"/>
+				<div class="projects">
+					<article>
+						<section>
+							<a href="projects/21.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/432239061_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/21.php" style="color:#00acf0;" id="project">[LV.2.1] Skyrii's...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/Melia.php" id="userproject">Melia</a>
+						</section>
+						<section>
+							<a href="projects/19.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/432161443_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/19.php" style="color:#00acf0;" id="project">Luctisity Disco...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/LuniFoxo.php" id="userproject">LuniFoxo</a>
+						</section>
+						<section>
+							<a href="projects/17.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/431354390_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/17.php" style="color:#00acf0;" id="project">Хорошая раб...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/Gohoski.php" id="userproject">Gohoski</a>
+						</section>
+						<section>
+							<a href="projects/16.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/431061872_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/16.php" style="color:#00acf0;" id="project">epicfunny</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/-ZEP-.php" id="userproject">-ZEP-</a>
+						</section>
+						<section>
+							<a href="projects/5.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/430161434_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/5.php" style="color:#00acf0;" id="project">Eihill's Intro...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/LuniFoxo.php" id="userproject">LuniFoxo</a>
+						</section>
+						<section>
+							<a href="projects/2.php"><img src="img/thumbnailsize.png" width="120px"></a><br/>
+							<a href="projects/2.php" style="color:#00acf0;" id="project">blank-2</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/LuniFoxo.php" id="userproject">LuniFoxo</a>
+						</section>
+					</article>
+				</div>
+			</div>
+			
+			<div class="projectscontainer" style="width:810px; display:block; margin-left:auto; margin-right:auto;">
+				<label id="title" style="color:#ccccff;">Recent Projects</label><br/>
+				<hr color="white"/>
+				<div class="projects">
+					<article>
+						<section>
+							<a href="projects/57.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/459619368_144x108.png" width="120px" height="80px" id="nsfwico"></a><br/>
+							<a href="projects/57.php" style="color:crimson;" id="project">После Фигни:...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+						<section>
+							<a href="projects/56.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/453706572_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/56.php" style="color:#00acf0;" id="project">После Фигни:...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+						<section>
+							<a href="projects/55.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/452698826_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/55.php" style="color:#00acf0;" id="project">После Фигни:...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+						<section>
+							<a href="projects/54.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/458898742_144x108.png" width="120px" height="80px" id="nsfwico"></a><br/>
+							<a href="projects/54.php" style="color:crimson;" id="project">Тест цензуры...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+						<section>
+							<a href="projects/53.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/369611156_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/53.php" style="color:#00acf0;" id="project">Майн Выжив...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+						<section>
+							<a href="projects/52.php"><img src="https://cdn2.scratch.mit.edu/get_image/project/458454933_144x108.png" width="120px" height="80px" id="projectico"></a><br/>
+							<a href="projects/52.php" style="color:#00acf0;" id="project">Тест проекта...</a><br/>
+							<a href="http://host3958.itelit.pro/luctisity/users/MasterGamescool.php" id="userproject">MasterGamescool</a>
+						</section>
+					</article>
+				</div>
+			</div>
+			
+			<div class="projectscontainer" style="width:810px; display:block; margin-left:auto; margin-right:auto;">
+				<label id="title" style="color:#ccccff;">Featured Studios</label><br/>
+				<hr color="white"/>
+				<div class="projects">
+					<article>
+						<section>
+							<a href=""><img src="img/thumbnailsize.png" width="120px"></a><br/>
+							<a href="" style="color:#00acf0;" id="project">studio</a><br/>
+						</section>
+					</article>
+				</div>
+			</div>
+			<div class="projectscontainer" style="width:810px; display:block; margin-left:auto; margin-right:auto;">
+				<label id="title" style="color:#ccccff;">Recently joined users</label><br/>
+				<hr color="white"/>
+				<div class="projects">
+					<article>
+						<section>
+							<a href="users/MasterGamescool.php"><img src="http://host3958.itelit.pro/luctisity/img/users/MasterGamescool.png" width="60px" height="60px"></a><br/>
+							<a href="users/MasterGamescool.php" style="color:#00acf0; font-size:12px;" id="project">MasterG...</a><br/>
+						</section>
+						<section>
+							<a href="users/Fraten_ghost.php"><img src="http://host3958.itelit.pro/luctisity/img/users/Fraten_ghost.png" width="60px" height="60px"></a><br/>
+							<a href="users/Fraten_ghost.php" style="color:#00acf0; font-size:12px;" id="project">Fraten_gh...</a><br/>
+						</section>
+						<section>
+							<a href="users/Scolder.php"><img src="http://host3958.itelit.pro/luctisity/img/users/Scolder.png" width="60px" height="60px"></a><br/>
+							<a href="users/Scolder.php" style="color:#00acf0; font-size:12px;" id="project">Scolder</a><br/>
+						</section>
+						<section>
+							<a href="users/Reknah.php"><img src="http://host3958.itelit.pro/luctisity/img/users/Reknah.png" width="60px" height="60px"></a><br/>
+							<a href="users/Reknah.php" style="color:#00acf0; font-size:12px;" id="project">Reknah</a><br/>
+						</section>
+						<section>
+							<a href="users/ness.php"><img src="http://host3958.itelit.pro/luctisity/img/users/ness.png" width="60px" height="60px"></a><br/>
+							<a href="users/ness.php" style="color:#00acf0; font-size:12px;" id="project">ness</a><br/>
+						</section>
+						<section>
+							<a href="users/Mario.php"><img src="http://host3958.itelit.pro/luctisity/img/users/Mario.png" width="60px" height="60px"></a><br/>
+							<a href="users/Mario.php" style="color:#00acf0; font-size:12px;" id="project">Mario</a><br/>
+						</section>
+						<section>
+							<a href="users/russia11.php"><img src="http://host3958.itelit.pro/luctisity/img/users/russia11.png" width="60px" height="60px"></a><br/>
+							<a href="users/russia11.php" style="color:#00acf0; font-size:12px;" id="project">russia11</a><br/>
+						</section>
+						<section>
+							<a href="users/Jack-Thander.php"><img src="http://host3958.itelit.pro/luctisity/img/users/Jack-Thander.png" width="60px" height="60px"></a><br/>
+							<a href="users/Jack-Thander.php" style="color:#00acf0; font-size:12px;" id="project">Jack-Th...</a><br/>
+						</section>
+						<section>
+							<a href="users/HockeyCat64.php"><img src="http://host3958.itelit.pro/luctisity/img/users/HockeyCat64.png" width="60px" height="60px"></a><br/>
+							<a href="users/HockeyCat64.php" style="color:#00acf0; font-size:12px;" id="project">HockeyC...</a><br/>
+						</section>
+						<section>
+							<a href="users/YeehawJunction.php"><img src="http://host3958.itelit.pro/luctisity/img/users/YeehawJunction.png" width="60px" height="60px"></a><br/>
+							<a href="users/YeehawJunction.php" style="color:#00acf0; font-size:12px;" id="project">YeehawJ...</a><br/>
+						</section>
+						<section>
+							<a href="users/DopoTSL.php"><img src="http://host3958.itelit.pro/luctisity/img/users/DopoTSL.png" width="60px" height="60px"></a><br/>
+							<a href="users/DopoTSL.php" style="color:#00acf0; font-size:12px;" id="project">DopoTSL</a><br/>
+						</section>
+						<section>
+							<a href="users/SackTime.php"><img src="http://host3958.itelit.pro/luctisity/img/users/SackTime.png" width="60px" height="60px"></a><br/>
+							<a href="users/SackTime.php" style="color:#00acf0; font-size:12px;" id="project">SackTime</a><br/>
+						</section>
+					</article>
+				</div>
+			</div>
+		</main><br/><br/><br/>
+		<footer>
+			<br/><h3 id="thicc" align="middle">Luniverse APS 2019-2020</h3>
+			<h4 id="thicc" align="middle">Powered by GHost</h3>
+		</footer>
+	</body>
+	<!-- <script>
+	function toggleMyProfile() {
+		var x = document.getElementById("selection");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+		} else {
+			x.style.display = "none";
+		}
+	}
+	</script> -->
+</html>
---
+++ script.js
@@ -0,0 +1,137 @@
+﻿var user = getCookie("user");
+var header =
+	`
+	<style>
+		#login {
+			display:none;
+			text-align:center;
+		}
+	
+		#logbuttons {
+			position:fixed;
+			right:10px;
+		}
+	
+		#ProfileMisc {
+			/*display:block;*/
+		}
+		
+		.headeracc {
+			display:block;
+		}
+		
+		#logbuttons>a {
+			padding:15px 10px 10px 10px;
+		}
+	</style>
+	<div class="header" onload="check()">
+			<a href="http://host3958.itelit.pro/luctisity/"><img src="http://host3958.itelit.pro/luctisity/img/icon2.png" width="150px" class="icon"></img></a>
+			<div class="searcharea">
+				<input type="search" placeholder="Search" class="search">
+				<img src="http://host3958.itelit.pro/luctisity/img/mag_glass.png" width="35px" class="searchbtn" onclick="reset()"></img>
+			</div>
+			<div class="headbuttons">
+				<a class="buttonhead" href><b style="color:#ccccff;">Create</b></a>
+				<a class="buttonhead" href><b style="color:#ccccff;">Explore</b></a>
+				<a class="buttonhead" ><b style="color:#ccccff;">Ideas</b></a>
+				<a class="buttonhead" href="http://host3958.itelit.pro/luctisity/credits.php"><b style="color:#ccccff;">About</b></a>
+			</div>
+			<div class="headbuttons" id="ProfileMisc" style="margin-top:17px;">
+				<a class="headbuttonsother"><img src="http://host3958.itelit.pro/luctisity/img/nav-notifications.png" width="30px" style="padding-top:3px;"></a>
+				<a class="headbuttonsother"><img src="http://host3958.itelit.pro/luctisity/img/mystuff.png" width="30px"></a>
+				<a class="headbuttonsother"><img src="http://host3958.itelit.pro/luctisity/img/settings.png" width="30px" style="padding-bottom:6px;"></a>
+			</div>
+			<div id="headeracc">
+				<!-- <li class="accountdrop"><a href="#"><img style="margin-bottom:-12px;" width="40px" src="img/defaultuser_60x60.png"> <label id="username"><b>demo_account</b></label></a>
+					  <ul class="dropdown">
+						<li id="accountdrop"><a href="">Профиль</a></li><br/>
+						<li id="accountdrop"><a href="">Мои работы</a></li><br/>
+						<li id="accountdrop"><a href="">Настройки учётной записи</a></li><br/>
+						<li id="accountdrop"><a href="">Выйти</a></li><br/>
+					  </ul>
+				</li> -->
+				<a href="http://host3958.itelit.pro/luctisity/users/`+getCookie("user")+`.php" id="MyProfileLink2"><img style="margin-bottom:-12px;" width="40px" height="40px" src="http://host3958.itelit.pro/luctisity/img/users/`+getCookie("user")+`.png"> <label id="username"><b style="color:#ccccff;">`+getCookie("user")+`</b></label></a>
+			</div>
+			<div id="logbuttons">
+				<a class="buttonlog" onclick="qlog()" style="cursor:pointer;"><b style="color:#ccccff;">Log In</b></a>
+			</div>
+		</div>
+		<br/><br/><br/><br/>
+		
+		<div id="login">
+			<div class="whitecontainer whitewarn">
+				<h1>Quick Log In</h1>
+				<label>Username:</label><input type="text" id="name"><br/><br/>
+				<button type="submit" onclick="submit()">Log in</button>
+				<button type="submit" onclick="off2()">Cancel</button>
+			</div>
+		</div>`
+
+document.write(header)
+check()
+
+function off() {
+	document.getElementById("warning").style.display = "none";
+}
+
+function back() {
+  window.history.back();
+}
+
+function off2() {
+	document.getElementById("login").style.display = "none";
+}
+
+function qlog() {
+	document.getElementById("login").style.display = "block";
+}
+
+function submit() {
+	var input = document.getElementById("name").value;
+	setCookie("user", input)
+	check();
+	location.reload();
+}
+
+function reset() {
+	setCookie("user", "")
+	location.reload();
+}
+
+function bruh() {
+	alert(user);
+	alert(getCookie("user"));
+}
+
+function setCookie(cname, cvalue) {
+  document.cookie = cname + "=" + cvalue + ";"
+}
+
+function getCookie(cname) {
+  var user = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(user) == 0) {
+      return c.substring(user.length, c.length);
+    }
+  }
+  return "";
+}
+
+function check() {
+	if (user != "") {
+		document.getElementById("ProfileMisc").style.display = "block";
+		document.getElementById("headeracc").style.display = "block";
+		document.getElementById("logbuttons").style.display = "none";
+		document.getElementById("username").innerHTML = getCookie("user");
+	} else {
+		document.getElementById("ProfileMisc").style.display = "none";
+		document.getElementById("headeracc").style.display = "none";
+		document.getElementById("logbuttons").style.display = "block";
+	}
+}
